6261a8b9a3cb2ac7c60fe5d5db9f15ffe129bd8e
LeeYoHan <o_o950306@naver.com>
2017-07-26 10:19:18 +0900

    10시

---
+++ JSProject/WebContent/Day3/Person.js
@@ -0,0 +1,102 @@
+/**
+ * 0726 9시~
+ */
+console.log('클래스 , 생성자 , 메서드 ');
+
+function Person(name){
+	this.name = name;
+}
+Person.prototype.setName = function(name){
+	this.name = name
+}
+Person.prototype.getName = function(){
+	return this.name;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ JSProject/WebContent/Day3/PersonTest.js
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+
+
+var tacademy1 = new Person('이요한');
+var tacademy2 = new Person('안성호');
+var tacademy3 = new Person('이문규');
+
+console.log(tacademy1.name);
+console.log(tacademy2.name);
+console.log(tacademy3.name);
+console.log(tacademy1.name);
+
+tacademy1.setName('민요한');
+console.log(tacademy1.getName());
+
+
+console.log(tacademy1.getName());
+
+
+
+
+
+
+
+
+
+
+
+
